826b1a4a4790b45be069291d37445c74b13e4219
kdiekroeger <62735098+kdiekroeger@users.noreply.github.com>
2022-10-14 10:42:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ def handler(event=None, context=None):
     options.add_argument("--remote-debugging-port=9222")
     chrome = webdriver.Chrome("/opt/chromedriver",
                               options=options)
-    chrome.get("https://example.com/")
+    chrome.get("https://www.cellartracker.com/notes.asp?iWine=3872386")
     return chrome.find_element(by=By.XPATH, value="//html").text
